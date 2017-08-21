b91fc459ba3068d8897c1fc332b863c083501016
George Wheeler <george@dewey4.com>
2017-08-21 09:30:44 -0700

    update

--- rethink/store.go
+++ rethink/store.go
@@ -1,9 +1,7 @@
 package rethinkmigrations
 
 import (
-	"fmt"
-	"log"
-	"strings"
+	"net"
 	"time"
 
 	"github.com/GoRethink/gorethink"
@@ -11,23 +9,36 @@ import (
 )
 
 // New returns a new rethink Migration
-func New(database string, session *gorethink.Session) (*Store, error) {
-	s := Store{
-		db:      gorethink.DB(database).Table("migrations"),
-		session: session,
-	}
+func New(host string, port string, database string) (*Store, error) {
+	emptyDBSession, err := gorethink.Connect(gorethink.ConnectOpts{
+		Address:  net.JoinHostPort(host, port),
+		Database: database,
+	})
 
-	mostRecent, _, err := s.MostRecent()
-	if err != nil && !strings.Contains(err.Error(), fmt.Sprintf("Database `%s` does not exist", database)) && !strings.Contains(err.Error(), "The result does not contain any more rows") {
-		return nil, err
-	}
+	var s Store
 
-	log.Printf("most rec: %d", mostRecent)
+	// mostRecent, _, err := s.MostRecent()
+	// if err != nil && !strings.Contains(err.Error(), fmt.Sprintf("Database `%s` does not exist", database)) && !strings.Contains(err.Error(), "The result does not contain any more rows") {
+	// 	return nil, err
+	// }
+	//
+	// log.Printf("most rec: %d", mostRecent)
+
+	var mostRecent int64
 
 	if mostRecent == 0 {
-		dbMig := CreateDB(session, database)
+		dbMig := CreateDB(emptyDBSession, database)
 
-		if err := dbMig.Migrate(); err != nil {
+		if err = dbMig.Migrate(); err != nil {
+			return nil, err
+		}
+
+		session, err := gorethink.Connect(gorethink.ConnectOpts{
+			Address:  net.JoinHostPort(host, port),
+			Database: database,
+		})
+
+		if err != nil {
 			return nil, err
 		}
 
@@ -37,6 +48,11 @@ func New(database string, session *gorethink.Session) (*Store, error) {
 			return nil, err
 		}
 
+		s = Store{
+			db:      gorethink.DB(database).Table("migrations"),
+			session: session,
+		}
+
 		if err := s.SaveMigration(migrations.Row{Order: 0, Key: dbMig.Key(0), Created: time.Now()}); err != nil {
 			return nil, err
 		}
@@ -44,6 +60,10 @@ func New(database string, session *gorethink.Session) (*Store, error) {
 		if err := s.SaveMigration(migrations.Row{Order: 1, Key: tableMig.Key(1), Created: time.Now()}); err != nil {
 			return nil, err
 		}
+	}
+
+	if err != nil {
+		return nil, err
 	}
 
 	return &s, nil
